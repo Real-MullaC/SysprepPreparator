5af837a8ab7b1fa57958e9a83c313aeb2a092d95
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2025-08-31 09:43:40 +0200

    [PrepTask] Add new Sysprep stopper PT

---
+++ Helpers/PreparationTasks/SysprepStopperPT.vb
@@ -0,0 +1,17 @@
+﻿Imports System.IO
+
+Namespace Helpers.PreparationTasks
+
+    Public Class SysprepStopperPT
+        Inherits PreparationTask
+
+        Public Overrides Function RunPreparationTask() As Boolean
+            ' We have to stop running copies of sysprep first before we can then run sysprep ourselves.
+            ' Failing to do this will return a message stating that sysprep is already running.
+            DynaLog.LogMessage("Stopping existing sysprep processes...")
+            Return RunProcess(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), "system32", "taskkill.exe"),
+                              "/F /IM sysprep.exe /T", HideWindow:=True, Inconditional:=True) = PROC_SUCCESS
+        End Function
+    End Class
+
+End Namespace
